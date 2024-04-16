ddeaefa065fe79f28bae68822df939c6387df1aa
Takku <farelvivo06@gmail.com>
2024-04-17 05:42:32 +0700

    added validator

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -2,19 +2,33 @@
 
 namespace Tests\Feature;
 
+use App\Rules\RegistrationRule;
+use App\Rules\Uppercase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\In;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function testValidator()
     {
-        $response = $this->get('/');
+        $data = [
+            "username" => "admin",
+            "password" => "123445"
+        ];
 
-        $response->assertStatus(200);
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
     }
 }
