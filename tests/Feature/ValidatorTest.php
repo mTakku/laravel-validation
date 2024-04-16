a3ec3eea9a490914432de8e7433a52a83cadfcf2
Takku <farelvivo06@gmail.com>
2024-04-17 06:12:38 +0700

    added function custom rule

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -217,4 +217,32 @@ class ValidatorTest extends TestCase
         Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 
+    public function testValidatorCustomFunctionRule()
+    {
+        $data = [
+            "username" => "farel@zeta.com",
+            "password" => "farel@zeta.com"
+        ];
+
+        $rules = [
+            "username" => ["required", "email", "max:100", function(string $attribute, string $value, \Closure $fail){
+                if(strtoupper($value) != $value){
+                    $fail("The field $attribute must be UPPERCASE");
+                }
+            }],
+            "password" => ["required", "min:6", "max:20", new RegistrationRule()]
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+
 }
