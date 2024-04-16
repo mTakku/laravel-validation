ac3496dff97032e7802524e5f0ee61159cce101b
Takku <farelvivo06@gmail.com>
2024-04-17 06:15:24 +0700

    added nested array validation

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -262,5 +262,32 @@ class ValidatorTest extends TestCase
         self::assertTrue($validator->passes());
     }
 
+    public function testNestedArray()
+    {
+        $data = [
+            "name" => [
+                "first" => "Farel",
+                "last" => "Mercys"
+            ],
+            "address" => [
+                "street" => "Jalan. Permata Cibubur",
+                "city" => "Bogor",
+                "country" => "Indonesia"
+            ]
+        ];
+
+        $rules = [
+            "name.first" => ["required", "max:100"],
+            "name.last" => ["max:100"],
+            "address.street" => ["max:200"],
+            "address.city" => ["required", "max:100"],
+            "address.country" => ["required", "max:100"],
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertTrue($validator->passes());
+
+    }
+
 
 }
