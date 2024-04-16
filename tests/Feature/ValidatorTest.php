40f1cc8c1923e020a7540cfdac8611c9e1d53601
Takku <farelvivo06@gmail.com>
2024-04-17 05:52:06 +0700

    added lang and validation message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -134,4 +134,34 @@ class ValidatorTest extends TestCase
             Log::error($message->toJson(JSON_PRETTY_PRINT));
         }
     }
+
+    public function testValidatorInlineMessage()
+    {
+        $data = [
+            "username" => "farel",
+            "password" => "farel"
+        ];
+
+        $rules = [
+            "username" => "required|email|max:100",
+            "password" => ["required", "min:6", "max:20"]
+        ];
+
+        $messages = [
+            "required" => ":attribute harus diisi",
+            "email" => ":attribute harus berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maksimal :max karakter",
+        ];
+
+        $validator = Validator::make($data, $rules, $messages);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
 }
