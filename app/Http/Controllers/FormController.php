7ef6f2b4377dec39b7fb172e2caad36617a87f43
Takku <farelvivo06@gmail.com>
2024-04-17 07:00:01 +0700

    added HTTP REQUEST VALIDATION

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+class FormController extends Controller
+{
+    public function form(): Response {
+        return response()->view("form");
+    }
+
+    public function submitForm(Request $request): Response
+    {
+        $data = $request->validate([
+        "username" => "required",
+        "password" => "required"
+        ]);
+
+        return response("OK", Response::HTTP_OK);
+    }
+    public function login(Request $request): Response
+    {
+        try {
+            $rules = [
+                "username" => "required",
+                "password" => "required"
+            ];
+
+            $data = $request->validate($rules);
+            // data
+            return response("OK", Response::HTTP_OK);
+        }catch (ValidationException $validationException){
+            return response($validationException->errors(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/form/login', [\App\Http\Controllers\FormController::class, 'login']);
+
+Route::get('/form', [\App\Http\Controllers\FormController::class, 'form']);
+Route::post('/form', [\App\Http\Controllers\FormController::class, 'submitForm']);
